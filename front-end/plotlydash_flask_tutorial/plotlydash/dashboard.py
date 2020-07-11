4d50de44791d29f30bfc5698e08b0289dd06c959
diegommora <diegommora@hotmail.com>
2020-07-11 18:14:43 -0500

    added create_map function

--- front-end/plotlydash_flask_tutorial/plotlydash/dashboard.py
+++ front-end/plotlydash_flask_tutorial/plotlydash/dashboard.py
@@ -5,7 +5,10 @@ import dash
 import dash_table
 import dash_html_components as html
 import dash_core_components as dcc
-from .data import create_dataframe, create_icbf_dataframe
+from urllib.request import urlopen
+import json
+import plotly.express as px
+from .data import create_dataframe, create_icbf_dataframe, create_desertion_dataframe
 from .layout import html_layout
 
 
@@ -23,6 +26,7 @@ def create_dashboard(server):
     # Load DataFrame
     df = create_dataframe()
     df_icbf = create_icbf_dataframe()
+    df_desertion = create_desertion_dataframe()
 
     # Custom HTML layout
     dash_app.index_string = html_layout
@@ -45,7 +49,9 @@ def create_dashboard(server):
                     'padding': 150
                 }
             }),
-            create_data_table(df_icbf)
+            create_data_table(df_icbf),
+            dcc.Graph(figure=create_map(df_desertion))
+            #create_map(df_desertion)
         ],
         id='dash-container'
     )
@@ -64,3 +70,23 @@ def create_data_table(df):
     )
     return table
 
+
+def create_map(df):
+    # Read the geojson file for Colombian Departments
+    with urlopen(
+            'https://gist.githubusercontent.com/john-guerra/43c7656821069d00dcbc/raw'
+            '/3aadedf47badbdac823b00dbe259f6bc6d9e1899/colombia.geo.json') as response:
+        depts = json.load(response)
+    for loc in depts['features']:
+        loc['id'] = loc['properties']['DPTO']
+
+    dep_map = px.choropleth_mapbox(df, geojson=depts, locations='code_dept', color='desertion',
+                                   color_continuous_scale="Viridis",
+                                   range_color=(1, 7),
+                                   mapbox_style="carto-positron",
+                                   zoom=4, center={"lat": 4.670128, "lon": -74.047078},
+                                   opacity=0.5,
+                                   labels={'desertion': 'desertion rate'}
+                                   )
+    dep_map.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0})
+    return dep_map
--- front-end/plotlydash_flask_tutorial/plotlydash/data.py
+++ front-end/plotlydash_flask_tutorial/plotlydash/data.py
@@ -29,5 +29,31 @@ def create_icbf_dataframe():
     df_icbf = run_query("""
     select * from educacion_basica_vars limit 100;""")
 
+    df_desertion = run_query("""
+    select code_dept, max(desertion_perc) as desertion
+    from desertion_by_municip
+    where year_cohort = 2019
+    group by code_dept
+    ;""")
+
     return df_icbf
 
+def create_desertion_dataframe():
+    DB_USERNAME = 'acastelblanco'
+    DB_PASSWORD = 'Team67!'
+    DB_ENDPOINT = 'ds4a-demo-instance.cqjr4hyu9xaq.us-east-1.rds.amazonaws.com'
+    DB_NAME = 'desertion_pj_team67'
+    engine = create_engine(f'postgresql://{DB_USERNAME}:{DB_PASSWORD}@{DB_ENDPOINT}/{DB_NAME}', max_overflow=20)
+
+    def run_query(sql):
+        result = engine.connect().execution_options(isolation_level="AUTOCOMMIT").execute((text(sql)))
+        return pd.DataFrame(result.fetchall(), columns=result.keys())
+
+    df_desertion = run_query("""
+    select code_dept, max(desertion_perc) as desertion
+    from desertion_by_municip
+    where year_cohort = 2019
+    group by code_dept
+    ;""")
+
+    return df_desertion
