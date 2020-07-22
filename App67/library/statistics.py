512c986b1913d7b9bc5d27367a8bf933e067355a
alejandracascru <alejandra.cas.cru@gmail.com>
2020-07-21 19:19:41 -0500

    mejora estadisticas con collapse

--- App67/library/statistics.py
+++ App67/library/statistics.py
@@ -3,8 +3,15 @@ import dash_core_components as dcc
 import pandas as pd
 import numpy as np
 
-import plotly.express as px
+import dash_bootstrap_components as dbc
+import dash_html_components as html
+from dash.dependencies import Input, Output, State
+
 import plotly.graph_objects as go
+
+from app import app
+
+import plotly.express as px
 from plotly.subplots import make_subplots
 
 ##############################
@@ -13,13 +20,13 @@ from plotly.subplots import make_subplots
 df_all = pd.read_csv('data/df_all.csv')
 
 
-def get_summary_variables(df_in, year, in_mun_code):
+def get_desercion_variables(df_in, year, in_mun_code):
     df_filtered = df_in[df_in['code_municip'] == in_mun_code]
     df_year = df_filtered[df_filtered['year_cohort'] == year]
 
     # desercion
     tempval = df_year['desertion_perc'].to_numpy()[0]
-    label_desercion = str(np.round(tempval, 2)) + ' %'
+    value_desercion = str(np.round(tempval, 2)) + ' %'
     y_plot = df_year[[
         'me_desercion_transicion',
         'me_desercion_primaria',
@@ -27,42 +34,62 @@ def get_summary_variables(df_in, year, in_mun_code):
         'me_desercion_secundaria',
     ]].to_numpy()
 
-    derc_perc = [x * tempval / sum(y_plot[0]) for x in y_plot[0]]
+    derc_perc = [np.round(x * tempval / sum(y_plot[0]), 2) for x in y_plot[0]]
 
+    return value_desercion, derc_perc
+
+
+def get_cobertura_variables(df_in, year, in_mun_code):
+    df_filtered = df_in[df_in['code_municip'] == in_mun_code]
+    df_year = df_filtered[df_filtered['year_cohort'] == year]
     # cobertura
     tempval_2 = df_year['me_cobertura_neta'].to_numpy()[0]
-    label_cobertura = str(np.round(tempval_2, 2)) + ' %'
+    value_cobertura = str(np.round(tempval_2, 2)) + ' %'
     y_plot_2 = df_year[[
         'me_cobertura_neta_transicion',
         'me_cobertura_neta_primaria',
         'me_cobertura_neta_media',
         'me_cobertura_neta_secundaria',
     ]].to_numpy()
+    cob_perc = y_plot_2[0]
+    return value_cobertura, cob_perc
 
-    cob_perc = [x * tempval_2 / sum(y_plot_2[0]) for x in y_plot_2[0]]
-    return label_desercion, derc_perc, label_cobertura, cob_perc
 
-
-def make_summary_fig(name_municipio,  name_depto, year, label_desercion, derc_perc, label_cobertura, cob_perc):
+def make_donut_desertion_fig(name_municipio, name_depto, year, label_desercion, derc_perc):
     labels = ['Transición', 'Primaria', 'Media', 'Secundaria']
-    sum_fig = make_subplots(rows=1, cols=2, specs=[[{'type': 'domain'}, {'type': 'domain'}]])
-    sum_fig.add_trace(go.Pie(labels=labels, values=derc_perc, name="Desercion",
-                             title="Deserción:" + label_desercion), 1, 1)
-    sum_fig.add_trace(go.Pie(labels=labels, values=cob_perc, name="Aprobacion",
-                             title='Cobertura:' + label_cobertura), 1, 2)
+    fig = go.Figure(data=[go.Pie(labels=labels,
+                                 values=derc_perc,
+                                 hole=.5,
+                                 hoverinfo="label+value"
+                                 )
+                                 ])
+    fig.update_layout(title_text="Deserción Total Población Escolar: " + label_desercion)
+    return fig
 
-    # donut-like pie chart
-    sum_fig.update_traces(hole=.4, hoverinfo="label+percent+name")
-    sum_fig.update_layout(
-        title_text="Deserción y Cobertura Escolar - " + str(year) + ' - ' + name_municipio[0] + ' - ' + name_depto[0])
-    return sum_fig
+
+def make_bar_cobertura_fig(name_municipio, name_depto, cob_perc, value_cobertura):
+    labels = ['Transición', 'Primaria', 'Media', 'Secundaria']
+    fig = go.Figure(data=[go.Bar(x=labels,
+                                 y=cob_perc,
+                                 )])
+    fig.update_layout(title_text="Cobertura Total Población Escolar: " + value_cobertura)
+    return fig
 
 
 def figure_desertion_year(df_all, selected_code, name_municipio):
     df_mun = df_all[df_all['code_municip'] == selected_code]
     result_fig = go.Figure(data=go.Scatter(x=df_mun['year_cohort'],
                                            y=df_mun['desertion_perc']
-                                           ))
+                                           ),
+                           )
+
+    result_fig.update_layout({
+        'plot_bgcolor': 'rgba(0, 0, 0, 0)',
+        'paper_bgcolor': 'rgba(0, 0, 0, 0)',
+    })
+
+    result_fig.update_layout(title_text="Deserción 2011-2019")
+
     return result_fig
 
 
@@ -71,29 +98,154 @@ def figure_desertion_year(df_all, selected_code, name_municipio):
 selected_code = 5001
 selected_year = 2019
 
-label_desercion, derc_perc, label_cobertura, cob_perc = get_summary_variables(df_in=df_all,
-                                                                              year=2019,
-                                                                              in_mun_code=selected_code,
-                                                                              )
-
 df_mun = df_all[df_all['code_municip'] == selected_code]
 name_municipio = df_mun.iloc[[0]]['name_dept'].to_numpy()
 name_depto = df_mun.iloc[[0]]['name_municip'].to_numpy()
 
-Summary_Fig = make_summary_fig(name_municipio=name_municipio,
-                               name_depto=name_depto,
-                               year=selected_year,
-                               label_desercion=label_desercion,
-                               derc_perc=derc_perc,
-                               label_cobertura=label_cobertura,
-                               cob_perc=cob_perc)
+label_desercion, derc_perc = get_desercion_variables(
+    df_in=df_all,
+    year=selected_year,
+    in_mun_code=selected_code,
+)
 
-Years_fig = figure_desertion_year(df_all, selected_code, name_municipio)
+value_cobertura, cob_perc = get_cobertura_variables(
+    df_in=df_all,
+    year=selected_year,
+    in_mun_code=selected_code,
+)
+
+PieFig = make_donut_desertion_fig(
+    name_municipio=name_municipio,
+    name_depto=name_depto,
+    year=selected_year,
+    label_desercion=label_desercion,
+    derc_perc=derc_perc,
+)
+
+BarFig = make_bar_cobertura_fig(
+    name_municipio=name_municipio,
+    name_depto=name_depto,
+    cob_perc=cob_perc,
+    value_cobertura=value_cobertura
+)
+
+Years_fig = figure_desertion_year(
+    df_all=df_all,
+    selected_code=selected_code,
+    name_municipio=name_municipio)
 
 ##############################
-#Map Layout
+# Layout
 ##############################
-statistics = html.Div([
-    dcc.Graph(figure=Summary_Fig, id='summary-figure'),
-    dcc.Graph(figure=Years_fig, id='year-figure'),
-], className="ds4a-body")
+
+explore_municipio = html.Div(
+    [
+        dbc.Row(
+            [
+                dbc.Col(
+                    html.H3(""),
+                    align="start",
+                    width=2
+                ),
+                dbc.Col(
+                    html.H3("Explorar Deserción y Cobertura por Municipio"),
+                    align="center",
+                ),
+            ]
+        ),
+
+        dbc.Row(
+            [
+                dbc.Col(
+                    html.Div("Dropdown Select Municipio"), width=2
+                ),
+                dbc.Col(
+                    dcc.Graph(figure=PieFig, id='Pie_d'), width=3
+                ),
+                dbc.Col(
+                    dcc.Graph(figure=Years_fig, id='Years_d'), width=4
+                ),
+                dbc.Col(
+                    dcc.Graph(figure=BarFig, id='Bar_c'), width=3
+                ),
+            ],
+            align="center",
+        ),
+    ]
+)
+
+explore_correlation = html.Div(
+    [
+        dbc.Row(
+            [
+                dbc.Col(
+                    html.H3(""),
+                    align="start",
+                    width=2
+                ),
+                dbc.Col(
+                    html.H3("Explorar Correlación entre Variables"),
+                    align="start",
+                ),
+            ]
+        ),
+        dbc.Row(
+            [
+                dbc.Col(
+                    html.Div(""), width=1
+                ),
+                dbc.Col(
+                    html.Div("Dropdown Select Variables"), width=1
+                ),
+                dbc.Col(
+                    dcc.Graph(figure=Years_fig, id='Years2'), width=10
+                ),
+            ],
+            align="center",
+        ),
+    ]
+)
+
+
+dbc.Col(html.Div("One of three columns"), width=3),
+statistics = html.Div(
+    [
+        dbc.Row(
+            explore_correlation
+        ),
+
+        dbc.Row(dbc.Col(
+            html.Div("")
+        )),
+
+        dbc.Button(
+            "Explorar Deserción y Cobertura por Municipio",
+            id="collapse-button",
+            className="mb-3",
+            color="secondary",
+        ),
+        dbc.Collapse(
+            dbc.Card(dbc.CardBody(
+                explore_municipio
+            )),
+            id="collapse",
+        ),
+
+    ]
+)
+
+
+##############################
+# Callbacks
+##############################
+
+
+@app.callback(
+    Output("collapse", "is_open"),
+    [Input("collapse-button", "n_clicks")],
+    [State("collapse", "is_open")],
+)
+def toggle_collapse(n, is_open):
+    if n:
+        return not is_open
+    return is_open
