f2834bbc50f863b6f9ee431c9d89604902ce5952
Fabio A. Lagos G <fabio.a.lagos@gmail.com>
2020-07-22 16:03:40 -0500

    Benchmarking Layout
    Layout de la sección de eficiencia.

--- App67/library/benchmarking.py
+++ App67/library/benchmarking.py
@@ -1,5 +1,8 @@
 import json
+import numpy as np
 import plotly.express as px
+import dash_table as dt
+import dash_core_components as dcc
 import dash_html_components as html
 
 from library import def_data
@@ -10,43 +13,132 @@ from library.elements_all import sidebar
 ##############################
 # CONTENTS
 # 1. Styles
-# 3. SQL queries
-# 4. Map
-# 5. Ranking
-# 6. DMU Groups
-# 7. Inp. Variables
+# 2. SQL queries
+# 3. Map
+# 4. Ranking
+# 5. DMU Groups
+# 6. Slacks/Waste
+# 7. Layout
 # ------------------------------
 
+# ------------------------------
+# 1. Styles
+# ------------------------------
+# 1.1 Map Styles
+MAP_BENCHMARK_STYLE = {
+    "position": "fixed",
+    "width": "35%",
+    "left": "17rem",
+    "top": "140px",
+    "border": "1px solid #e7eff6",
+    "border-radius": "10px"
+}
+
+# 1.2 Ranking Table Styles
+RANKING_TABLE_BENCHMARK_STYLE = {
+    "position": "fixed",
+    "width": "20%",
+    "right": "1rem",
+    "top": "140px",
+    "border": "1px solid #e7eff6",
+    "border-radius": "10px"
+}
+
+# 1.3 Group Table Styles
+GROUP_TABLE_BENCHMARK_STYLE = {
+    "position": "fixed",
+    "width": "20%",
+    "right": "22%",
+    "top": "140px",
+    "border": "1px solid #e7eff6",
+    "border-radius": "10px"
+}
+
+# 1.4 Group Table Styles
+SLACK_GRAPH_BENCHMARK_STYLE = {
+    "position": "fixed",
+    "width": "41%",
+    "height": "200px",
+    "right": "1rem",
+    "top": "480px",
+    "border": "1px solid #e7eff6",
+    "border-radius": "10px"
+}
 
 # ------------------------------
 # 2. SQL Queries
 # ------------------------------
-# 2.1 Query variables
-#df_dropout_efficiency = def_data.runQuery("""select code_municip, mtbm.name_municip as muni, mtbm.dane_alu_01 as alumns, (mtbm.dane_alu_01 - mtbm.dane_alu_11) as nodropouts
-#    from master_table_by_municipio mtbm
-#    where mtbm.year_cohort = 2019
-#    and region = 'Caribe'
-#    and mtbm.dane_alu_01 is not null
-#    and mtbm.dane_alu_11 is not null
-#    and mtbm.dane_alu_01 > 0;
-#    ;""")
+# 2.1 Initial query
+# ------------------------------
+df_dropout_efficiency = def_data.runQuery("""
+    select code_municip, mtbm.dane_alu_11 as nodropouts
+    from master_table_by_municipio mtbm 
+    where mtbm.year_cohort = 2019 
+    and mtbm.dane_alu_01 is not null 
+    and mtbm.dane_alu_11 is not null
+    and mtbm.dane_alu_01 > 0;""")
+df_dropout_efficiency['nodropouts'] = df_dropout_efficiency['nodropouts'].astype(np.float64)
+# 2.1 Query function
+# ------------------------------
+
 # ------------------------------
 # 3. Map
 # ------------------------------
-#with open('/Users/alberto/Documents/GitHub/dS4A_team67/App67/data/MGN_MPIO_POLITICO.json') as geo:
-#    munijson = json.loads(geo.read())
-#EF_Map = px.choropleth_mapbox(df_dropout_efficiency,                         #Data
-#        locations='code_municip',                   #Column containing the identifiers used in the GeoJSON file
-#        featureidkey="properties.MPIO_CCNCT",
-#        color='nodropouts',                         #Column giving the color intensity of the region
-#        geojson=munijson,                           #The GeoJSON file
-#        zoom=4,                                     #Zoom
-#        mapbox_style="carto-positron",              #Mapbox style, for different maps you need a Mapbox account and a token
-#        center={"lat": 4.0902, "lon": -75.7129},    #Center
-#        color_continuous_scale="Viridis",           #Color Scheme
-#        opacity=0.5                                 #Opacity of the map
-#        )
+# 3.1 Loads JSON file
+# ------------------------------
+with open('data/municipios95.json') as geo:
+    munijson = json.loads(geo.read())
 
+# 3.2 Define initial map properties
+# ------------------------------
+EF_Map = px.choropleth_mapbox(df_dropout_efficiency,     # Data
+        locations='code_municip',                # Column containing the identifiers used in the GeoJSON file
+        featureidkey="properties.MPIO_CCNCT",    # Column in de JSON containing the identifier of the municipality.
+        color='nodropouts',                      # Column giving the color intensity of the region
+        geojson=munijson,                        # The GeoJSON file
+        zoom=4,                                  # Zoom
+        mapbox_style="white-bg",           # Mapbox style, for different maps you need a Mapbox account and a token
+        center={"lat": 4.5709, "lon": -74.2973}, # Center
+        color_continuous_scale="Viridis",        # Color Scheme
+        opacity=0.5                              # Opacity of the map
+        )
+EF_Map.update_geos(fitbounds="locations", visible=False)
+EF_Map.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+
+# ------------------------------
+# 4. Ranking
+# ------------------------------
+ranking_table = dt.DataTable(
+    id='table',
+    columns=[{"name": i, "id": i} for i in df_dropout_efficiency.columns],
+    data=df_dropout_efficiency.to_dict('records'),
+    style_table={'height': '280px', 'overflowY': 'auto'}
+)
+
+# ------------------------------
+# 5. DMU Groups
+# ------------------------------
+group_table = dt.DataTable(
+    id='table',
+    columns=[{"name": i, "id": i} for i in df_dropout_efficiency.columns],
+    data=df_dropout_efficiency.to_dict('records'),
+    style_table={'height': '280px', 'overflowY': 'auto'}
+)
+
+# ------------------------------
+# 6. Slack/Waste
+# ------------------------------
+
+slack_graph = px.histogram(df_dropout_efficiency, x="nodropouts", height=200)
+
+# ------------------------------
+# 7. Layout
+# ------------------------------
 benchmarking = html.Div([
-    sidebar.sidebar
+    sidebar.sidebar,
+    html.Div([dcc.Graph(figure=EF_Map, id='US_map')],style = MAP_BENCHMARK_STYLE),
+    html.Div([ranking_table],style = RANKING_TABLE_BENCHMARK_STYLE),
+    html.Div([group_table],style = GROUP_TABLE_BENCHMARK_STYLE),
+    html.Div([dcc.Graph(figure=slack_graph, id='slack_graph')],style = SLACK_GRAPH_BENCHMARK_STYLE)
+
 ], className="ds4a-body")
